8731fbe73ecb52edcf7ec85197a6459ba033e7c1
Dani's Fan <101030561+CoolProgrammer2052@users.noreply.github.com>
2022-03-06 20:52:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+someone = input("Hello! How May I Help You? ")
+if someone == "karlson":
+    print("Oh! So you don't know what karlson is, Karlson is a game which I am currently working on it's on the top 18 right now so make sure to wishlist it GAMERZ!")
+else:
+    print("What bro?")
